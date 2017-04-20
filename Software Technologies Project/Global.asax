b2f31d1cc080ea5a1f5e27cccf44de76bb459950
IvanNiv <vankoni68@gmail.com>
2017-04-21 00:21:44 +0300

    Blog Main Functionality

---
+++ Software Technologies Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Software_Technologies_Project.MvcApplication" Language="C#" %>
---
+++ Software Technologies Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Software_Technologies_Project.Startup))]
+namespace Software_Technologies_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
