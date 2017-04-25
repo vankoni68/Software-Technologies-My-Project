60287778c5eba331d6be3c3780b1d3ce01aed8da
IvanNiv <vankoni68@gmail.com>
2017-04-25 23:57:02 +0300

    Added new Models/Skizone.cs, Changed in Models/BlogDbContext.cs

--- Blog/Models/BlogDbContext.cs
+++ Blog/Models/BlogDbContext.cs
@@ -16,6 +16,8 @@ namespace Blog.Models
 
         public DbSet<Article> Articles { get; set; }
 
+        public virtual IDbSet<Skizone> Skizones { get; set; }
+
         public static BlogDbContext Create()
         {
             return new BlogDbContext();
---
+++ Blog/Models/Skizone.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class Skizone
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [Display(Name = "Elevation info (min m - max m)")]
+        public string ElevationInfo { get; set; }
+
+        [Required]
+        [Range(0.0, 999.9)]
+        [Display(Name = "Slopes in km.")]
+        public double Slopes { get; set; }
+
+        [Required]
+        [Range(0.0, 99.9)]
+        [Display(Name = "Lift ticket in €")]
+        public double LiftTicket { get; set; }
+
+        [Required]
+        [Display(Name = "Info")]
+        public string ContentInfo { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+
+        public bool IsAuthor(string authorId)
+        {
+            return this.AuthorId == authorId;
+        }
+    }
+
+
+}
