5d5cb4db38e54bd136d41f7676da0ccab4a457d9
IvanNiv <vankoni68@gmail.com>
2017-04-29 12:44:26 +0300

    Added Tag Functionality. Added Models/Tag.cs

---
+++ Blog/Models/Tag.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class Tag
+    {
+        private ICollection<Skizone> skizones;
+
+        public Tag()
+        {
+            this.skizones = new HashSet<Skizone>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Index(IsUnique = true)]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Skizone> Skizones
+        {
+            get { return this.skizones; }
+            set { this.skizones = value; }
+        }
+    }
+}
